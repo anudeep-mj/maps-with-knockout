24f4651c1de2e871391055cbf514fe21bca30a2d
i851226 <anudeep.matta.jayaram@sap.com>
2017-07-03 12:43:50 -0700

    Commit before review

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Maps with knockout
+## Synopsis
+
+This project is a small app which displays few different locations in the map which can be be played around with.
+Project shows the functionality of the following concepts:
+- knockout.js
+- google maps api
+- new york times api
+- mvvm model 
+
+Compatible on desktop, tablet and mobile.
+
+## Installation
+
+Git clone the project and open the index.html in a browser.
+
+## Functionalities
+
+- Filter dropdown at the top which filters the locations and drops a marker on the filtered location.
+- Clicking on any of the location in the list drops a pin on the clicked location
+- Clicking on the pin shows a bounce effect and a small pin-popup with the place name
+- Clicking on a pin shows a small window below which displays top 10 results of the new york times related to the place
--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 var locations = [
-          {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-          {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-          {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-          {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-          {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-          {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+    {title: '230 Fifth Rooftop Bar', location: {lat: 40.744390, lng: -73.988327}},
+    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
 ];
 
 var map;
@@ -14,7 +14,6 @@ function initMap() {
     var i = 0;
     var self = this;
     var bounds = new google.maps.LatLngBounds();
-    var largeInfowindow = new google.maps.InfoWindow();
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.7413549, lng: -73.9980244},
@@ -23,140 +22,158 @@ function initMap() {
 
     loc = {lat: 40.7713024, lng: -73.9632393};
 
-    locations.forEach(function(locationItem) {
-    	var newMarker = initmarkers(locationItem.location)
-    	markersArray.push(newMarker);
-    	// newMarker.addListener('click', function() {
-     //        populateInfoWindow(this, largeInfowindow);
-     //    });
+    locations.forEach(function (locationItem) {
+        var newMarker = initmarkers(locationItem)
+        markersArray.push(newMarker);
+        initPopup(newMarker);
+        enableBounce(newMarker);
 
-    	bounds.extend(markersArray[i++].position);
+        bounds.extend(markersArray[i++].position);
     });
 
     map.fitBounds(bounds);
 }
 
+function googleError() {
+    alert("Something went wrong! Google maps not loaded.");
+};
 
-function initmarkers(location){
-	this.marker = new google.maps.Marker({
-          position: location,
-          map: map,
-          title: 'asdf',
-          animation: google.maps.Animation.DROP,
+//Add bounce affect to a marker location
+function enableBounce(marker) {
+    marker.addListener('click', function () {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    });
+}
+
+//Returns a marker for a given location object
+function initmarkers(location) {
+    this.marker = new google.maps.Marker({
+        position: location.location,
+        map: map,
+        title: location.title,
+        animation: google.maps.Animation.DROP,
     });
 
     return this.marker;
 }
 
+//Initializes popup windows for the markers
+function initPopup(marker) {
+    var largeInfowindow = new google.maps.InfoWindow();
+    marker.addListener('click', function () {
+        populateInfoWindow(this, largeInfowindow);
+    });
+}
+
+//Clears all the markers
 function clearOverlays() {
-	for (var i = 0; i < markersArray.length; i++ ) {
-		markersArray[i].setMap(null);
-	}
-  markersArray.length = 0;
+    for (var i = 0; i < markersArray.length; i++) {
+        markersArray[i].setMap(null);
+    }
+    markersArray.length = 0;
 }
 
 function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
+    // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick',function(){
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function () {
             infowindow.setMarker = null;
-       	});
+        });
     }
 }
 
-var viewModel = function() {
-	var self = this;
-	var i = 0;
-	this.locationList = ko.observableArray([]);
-	this.myObservableArray = ko.observableArray();
-        
+var viewModel = function () {
+    var self = this;
+    var i = 0;
+    this.locationList = ko.observableArray([]);
+    this.myObservableArray = ko.observableArray();
+    self.articleList = ko.observableArray();
+
     self.selectedValue = ko.observable();
     self.myObservableArray.push({title: 'Choose a marker'});
     self.myObservableArray.push({title: 'All'});
 
 
-	locations.forEach(function(locationItem, map) {
-		self.locationList.push( new Location(locationItem)) ;
-		// self.myObservableArray.push(locationItem.location);
-		self.myObservableArray.push(new Location(locationItem));
-	});
+    locations.forEach(function (locationItem, map) {
+        self.locationList.push(new Location(locationItem));
+        self.myObservableArray.push(new Location(locationItem));
+    });
 
-	self.selectedValue.subscribe(function(newValue) {
-		//console.log(newValue.title());
-			
-		//title = newValue.title();
-		console.log(newValue);
-		if(newValue.title !== 'Choose a marker' && newValue.title !== 'All') {
-			lateral = newValue.lat();
-			lngtd = newValue.lng();
-			title = newValue.title();
-			newValue = {lat: lateral, lng: lngtd};
-			clearOverlays();
-			marker = initmarkers(newValue);
-			markersArray.push(marker);
-		}
+    self.selectedValue.subscribe(function (newValue) {
+        if (newValue.title !== 'Choose a marker' && newValue.title !== 'All') {
+            lateral = newValue.lat();
+            lngtd = newValue.lng();
+            title = newValue.title();
+            newValue = {lat: lateral, lng: lngtd};
+            clearOverlays();
+            marker = initmarkers({title: title, location: newValue});
+            initPopup(marker);
+            enableBounce(marker);
+            markersArray.push(marker);
+        }
 
-		else if(newValue.title == 'All') {
-			clearOverlays();
-			locations.forEach(function(locationItem) {
-    			largeInfowindow = new google.maps.InfoWindow();
-    			var newMarker = initmarkers(locationItem.location)
-    			markersArray.push(newMarker);
-    		});
-		}
-	});
+        else if (newValue.title == 'All') {
+            clearOverlays();
+            locations.forEach(function (locationItem) {
+                largeInfowindow = new google.maps.InfoWindow();
+                var newMarker = initmarkers(locationItem)
+                initPopup(newMarker);
+                enableBounce(newMarker);
+                markersArray.push(newMarker);
+            });
+        }
+    });
 
-	function currentLoc(loc) {
-		this.loc.addListener('click', function() {
-			populateInfoWindow(this, largeInfowindow);
-		})
-	}
-
-	this.setLocation = function(clickedLocation) {
-		var largeInfowindow = new google.maps.InfoWindow();
-		clearOverlays();
-		console.log(clickedLocation)
-		console.log(clickedLocation.title())
-		//console.log(clickedLocation.lat())
-		lateral = clickedLocation.lat();
-		lngtd = clickedLocation.lng();
-		loc = {lat: lateral, lng: lngtd};
-		console.log(loc);
-		var marker = new google.maps.Marker({
+    this.setLocation = function (clickedLocation) {
+        var largeInfowindow = new google.maps.InfoWindow();
+        clearOverlays();
+        lateral = clickedLocation.lat();
+        lngtd = clickedLocation.lng();
+        loc = {lat: lateral, lng: lngtd};
+        console.log(loc);
+        var marker = new google.maps.Marker({
             map: map,
             position: loc,
             title: clickedLocation.title(),
             animation: google.maps.Animation.DROP,
         });
         markersArray.push(marker);
-        marker.addListener('click', function() {
+        enableBounce(marker);
+        marker.addListener('click', function () {
             populateInfoWindow(this, largeInfowindow);
         });
 
-		//marker = initmarkers(loc);
-		//alert('yay')
-		//currentLoc(clickedLocation)
-	}
+        tempfunc = function () {
+            var nytimeurl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + clickedLocation.title() + '&sort=newest&api-key=dfb69fab279a46ce8919e44988a3db76';
+            $.getJSON(nytimeurl, function (data) {
+                articles = data.response.docs;
+                for (var i = 0; i < articles.length; i++) {
+                    var article = articles[i]
+                    self.articleList.push({url: article.web_url, headline: article.headline.main});
+                }
+                ;
+                console.log(self.articleList());
+            }).error(function () {
+                self.articleList.push("Something went wrong!");
+            });
+        }
+        tempfunc();
+        self.articleList([]);
+    }
 }
 
-var Location = function(data) {
-	this.lat = ko.observable(data.location.lat);
-	this.lng = ko.observable(data.location.lng);
-	this.title = ko.observable(data.title);
-	//console.log(data.title);
-	// this.marker = new google.maps.Marker({
- //          position: data.location,
- //          map: map,
- //          title: data.title,
- //          animation: google.maps.Animation.DROP,
- //    });
-    // this.marker.addListener('click', function() {
-    //     populateInfoWindow(this, largeInfowindow);
-    // });
+var Location = function (data) {
+    this.lat = ko.observable(data.location.lat);
+    this.lng = ko.observable(data.location.lng);
+    this.title = ko.observable(data.title);
 }
 
 
